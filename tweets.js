208e44bd84660349ff69dfef4d091992b2bb42b7
Oliver Barnwell <barnwelloliver@gmail.com>
2019-10-20 14:56:59 +0100

    add some more memes

--- tweets.js
+++ tweets.js
@@ -3,8 +3,26 @@ const stringys = [
 	`$2 is the $1 killer`,
 	`remember when we used $1 instead of $2?`,
 	`Using $2 to Replace $1`,
+	`Not long until $2 reaches critical mass and decimates $1`,
+	`Who would use $1 over $2 in ${new Date().getFullYear()}?!`,
+	`
+		woke: $2
+		broke: $1 
+
+		There I said it.
+	`,
+	`
+	|￣￣￣￣￣￣| 
+	| Use       | 
+	|    $2      | 
+	| OVER   |
+	|     $1     | 
+	|＿＿＿＿＿＿| 
+	 (\__/)  || 
+	 (•ㅅ•) || 
+	 / 　 づ`
 ];
 
-const extras = ['LOL', '😻', '😜', '🥰', 'New blogpost:'];
+const extras = ['LOL', '😻', '😜', '🥰', 'New blogpost:', 'We need to talk:', '🔥🔥🔥']
 
 module.exports = { stringys, extras };
